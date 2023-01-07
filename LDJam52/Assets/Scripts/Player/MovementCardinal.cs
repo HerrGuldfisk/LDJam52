6cc5fd50cb9bc7fda3a1a1d9f57db2fdd23c04dd
Theodor <telo_tennis@hotmail.com>
2023-01-07 11:34:00 +0100

    Player data

--- LDJam52/Assets/Scripts/Player/MovementCardinal.cs
+++ LDJam52/Assets/Scripts/Player/MovementCardinal.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class MovementCardinal : MonoBehaviour
 {
+	PlayerData playerData;
+
     // Start is called before the first frame update
     void Start()
     {
-
+		playerData = GetComponent<PlayerData>();
     }
 
     // Update is called once per frame
---
+++ LDJam52/Assets/Scripts/Player/PlayerData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+	public float cardinalSpeedMAX = 5f;
+	public float cardinalAccelleration = 1.5f;
+
+	public float oxygenMax = 30f;
+}
