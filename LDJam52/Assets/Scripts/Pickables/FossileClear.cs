4ddff2aa0446048cba831927c9bbd589a71557f1
Theodor <telo_tennis@hotmail.com>
2023-01-09 01:09:10 +0100

    lates

---
+++ LDJam52/Assets/Scripts/Pickables/FossileClear.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FossileClear : Pickables
+{
+	public override int Index { get; set; } = 5;
+	public override int Value { get; set; } = 95;
+}
